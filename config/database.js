e8ee13b2ac3b0981fcae081a02657a53e9164536
FullStackAbbs <60401214+FullStackAbbs@users.noreply.github.com>
2020-03-27 22:02:06 -0400

    commit

---
+++ config/database.js
@@ -0,0 +1,8 @@
+// config/database.js
+module.exports = {
+
+// we inserted our Barista database.
+
+    'url' : 'mongodb+srv://check:check@cluster0-rmmvm.mongodb.net/test?retryWrites=true&w=majority', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': 'orders'
+};
